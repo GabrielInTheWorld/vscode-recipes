d380e07d597565373772004ea58672ae060a794a
GabrielMeyer <meyergabriel@live.de>
2020-03-23 15:50:49 +0100

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import * as http from 'http';
+import * as http from "http";
+
+import { h } from "./hello";
 
 let reqCnt = 1;
 
-http.createServer((req, res) => {
+http
+	.createServer((req, res) => {
+		const message = `Request Count: ${reqCnt}`;
 
-  const message = `Request Count: ${reqCnt}`;
+		res.writeHead(200, { "Content-Type": "text/html" });
+		res.end(
+			`<html><head><meta http-equiv="refresh" content="2"></head><body>${message}</body></html>`
+		);
 
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end(`<html><head><meta http-equiv="refresh" content="2"></head><body>${message}</body></html>`);
+		h();
+		h();
+		console.log("handled request: " + reqCnt++);
+	})
+	.listen(3000);
 
-  console.log("handled request: " + reqCnt++);
-}).listen(3000);
-
-console.log('server running on port 3000');
+console.log("Hello World");
+console.log("Hello World from docker");
+console.log("server running on port 3000");
